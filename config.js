7154fff01f6048e32ef0d42ec40062eff8c64bff
Hazlan Rozaimi <hazlan.rozaimi@mirumagency.com>
2017-07-19 16:05:15 +0800

    added config.js example

---
+++ config.js
@@ -0,0 +1,10 @@
+const appConfig = {
+  "accessKeyId": "",
+  "secretAccessKey": "",
+  "region": "us-east-1",
+  "apiVersion": "2016-11-28",
+  "botAlias": "",
+  "botName": "",
+  "userId": "",
+};
+export default appConfig;
